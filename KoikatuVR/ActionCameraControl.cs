9390a238d49be39296330773b5423f4ae2a7b05e
mosirnik <mosirnik@gmail.com>
2021-07-12 11:46:41 +0900

    Move TransformPatches out to a new file

--- KoikatuVR/ActionCameraControl.cs
+++ KoikatuVR/ActionCameraControl.cs
@@ -7,7 +7,6 @@ using VRGIN.Core;
 using HarmonyLib;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Diagnostics;
 using System.Collections;
 
 namespace KoikatuVR
@@ -249,35 +248,4 @@ namespace KoikatuVR
     {
         public static Transform targetTransform;
     }
-
-    /*
-    /// Report every time targetTransform is rotated. Useful during development.
-    [HarmonyPatch]
-    class TransformPatches
-    {
-        static IEnumerable<MethodBase> TargetMethods()
-        {
-            yield return AccessTools.PropertySetter(typeof(Transform), "rotation");
-            yield return AccessTools.PropertySetter(typeof(Transform), "localRotation");
-            yield return AccessTools.PropertySetter(typeof(Transform), "eulerAngles");
-            yield return AccessTools.Method(typeof(Transform), "SetPositionAndRotation");
-            yield return AccessTools.Method(typeof(Transform), "Rotate", new[] { typeof(Vector3), typeof(Space) });
-            yield return AccessTools.Method(typeof(Transform), "Rotate", new[] { typeof(float), typeof(float), typeof(float), typeof(Space) });
-            yield return AccessTools.Method(typeof(Transform), "Rotate", new[] { typeof(Vector3), typeof(float), typeof(Space) });
-            yield return AccessTools.Method(typeof(Transform), "RotateAround", new[] { typeof(Vector3), typeof(Vector3), typeof(float) });
-            yield return AccessTools.Method(typeof(Transform), "LookAt", new[] { typeof(Transform), typeof(Vector3) });
-            yield return AccessTools.Method(typeof(Transform), "LookAt", new[] { typeof(Vector3), typeof(Vector3) });
-            yield break;
-        }
-
-        static void Prefix(Transform __instance)
-        {
-            if (__instance == TransformDebug.targetTransform)
-            {
-                VRLog.Info("Someone is modifying the target transform!");
-                VRLog.Info(new StackTrace(false));
-            }
-        }
-    }
-    */
 }
---
+++ KoikatuVR/DebugHooks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Collection of patches that are to be enabled only during development.
+
+namespace KoikatuVR
+{
+#if false
+    /// Report every time TransformDebug.targetTransform is rotated.
+    [HarmonyPatch]
+    class TransformPatches
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.PropertySetter(typeof(Transform), "rotation");
+            yield return AccessTools.PropertySetter(typeof(Transform), "localRotation");
+            yield return AccessTools.PropertySetter(typeof(Transform), "eulerAngles");
+            yield return AccessTools.Method(typeof(Transform), "SetPositionAndRotation");
+            yield return AccessTools.Method(typeof(Transform), "Rotate", new[] { typeof(Vector3), typeof(Space) });
+            yield return AccessTools.Method(typeof(Transform), "Rotate", new[] { typeof(float), typeof(float), typeof(float), typeof(Space) });
+            yield return AccessTools.Method(typeof(Transform), "Rotate", new[] { typeof(Vector3), typeof(float), typeof(Space) });
+            yield return AccessTools.Method(typeof(Transform), "RotateAround", new[] { typeof(Vector3), typeof(Vector3), typeof(float) });
+            yield return AccessTools.Method(typeof(Transform), "LookAt", new[] { typeof(Transform), typeof(Vector3) });
+            yield return AccessTools.Method(typeof(Transform), "LookAt", new[] { typeof(Vector3), typeof(Vector3) });
+            yield break;
+        }
+
+        static void Prefix(Transform __instance)
+        {
+            if (__instance == TransformDebug.targetTransform)
+            {
+                VRLog.Info("Someone is modifying the target transform!");
+                VRLog.Info(new StackTrace(false));
+            }
+        }
+    }
+#endif
+}
