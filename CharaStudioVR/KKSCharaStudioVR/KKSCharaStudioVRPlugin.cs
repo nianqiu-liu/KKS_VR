3a238b5d187d735f2f6dc99b2345c740fc64bf34
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-01-25 01:30:18 +0100

    Allow using --vr in studio

--- CharaStudioVR/KKSCharaStudioVR/KKSCharaStudioVRPlugin.cs
+++ CharaStudioVR/KKSCharaStudioVR/KKSCharaStudioVRPlugin.cs
@@ -6,51 +6,51 @@ using VRGIN.Core;
 
 namespace KKSCharaStudioVR
 {
-	[BepInProcess("CharaStudio")]
-	[BepInPlugin("KKS_CharaStudioVR", "KKS_CharaStudioVR", "0.0.2")]
-	public class KKSCharaStudioVRPlugin : BaseUnityPlugin
-	{
-		public const string GUID = "KKS_CharaStudioVR";
+    [BepInProcess("CharaStudio")]
+    [BepInPlugin("KKS_CharaStudioVR", "KKS_CharaStudioVR", "0.0.2")]
+    public class KKSCharaStudioVRPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "KKS_CharaStudioVR";
 
-		public const string NAME = "KKS_CharaStudioVR";
+        public const string NAME = "KKS_CharaStudioVR";
 
-		public const string VERSION = "0.0.2";
+        public const string VERSION = "0.0.2";
 
-		private static ManualLogSource defaultLogger;
+        private static ManualLogSource defaultLogger;
 
-		public bool vrActivated;
+        public bool vrActivated;
 
-		public static ManualLogSource PluginLogger => defaultLogger;
+        public static ManualLogSource PluginLogger => defaultLogger;
 
-		public KKSCharaStudioVRPlugin()
-		{
-			vrActivated = Environment.CommandLine.Contains("--studiovr");
-			defaultLogger = base.Logger;
-			if (vrActivated)
-			{
-				OpenVRHelperTempfixHook.Patch();
-				SceneManager.sceneLoaded += OnSceneLoaded;
-			}
-			else
-			{
-				base.Logger.LogInfo("Ignore loading VR. To load VR, specify '--studiovr' in commandline option.");
-			}
-		}
+        public KKSCharaStudioVRPlugin()
+        {
+            vrActivated = Environment.CommandLine.Contains("--vr") || Environment.CommandLine.Contains("--studiovr");
+            defaultLogger = base.Logger;
+            if (vrActivated)
+            {
+                OpenVRHelperTempfixHook.Patch();
+                SceneManager.sceneLoaded += OnSceneLoaded;
+            }
+            else
+            {
+                base.Logger.LogInfo("Ignore loading VR. To load VR, specify '--studiovr' in commandline option.");
+            }
+        }
 
-		private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-		{
-			defaultLogger.LogDebug($"Scene Loaded {scene.name}, {mode}");
-			if (mode == LoadSceneMode.Single)
-			{
-				VRLog.Debug("Loaded Scene is " + scene.name);
-				if (vrActivated && "Studio" == scene.name)
-				{
-					VRLoader.Create(true);
-					SaveLoadSceneHook.InstallHook();
-					LoadFixHook.InstallHook();
-					VRLog.Level = VRLog.LogMode.Info;
-				}
-			}
-		}
-	}
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            defaultLogger.LogDebug($"Scene Loaded {scene.name}, {mode}");
+            if (mode == LoadSceneMode.Single)
+            {
+                VRLog.Debug("Loaded Scene is " + scene.name);
+                if (vrActivated && "Studio" == scene.name)
+                {
+                    VRLoader.Create(true);
+                    SaveLoadSceneHook.InstallHook();
+                    LoadFixHook.InstallHook();
+                    VRLog.Level = VRLog.LogMode.Info;
+                }
+            }
+        }
+    }
 }
