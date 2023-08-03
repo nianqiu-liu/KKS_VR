mosirnik/KK_MainGameVR

Fix handling of two consecutive H scenes (untested)

KoikatuVR/Interpreters/HSceneInterpreter.cs
@@ -7,7 +7,8 @@ namespace KoikatuVR.Interpreters
 {
     class HSceneInterpreter : SceneInterpreter
     {
-        private bool _initialized;
+        bool _active;
+        HSceneProc _proc;
         Caress.VRMouth _vrMouth;
 
         public override void OnStart()
@@ -16,23 +17,38 @@ namespace KoikatuVR.Interpreters
 
         public override void OnDisable()
         {
-            if (_initialized)
-            {
-                GameObject.Destroy(_vrMouth);
-                DestroyControllerComponent<Caress.CaressController>();
-            }
+            Deactivate();
         }
 
         public override void OnUpdate()
         {
-            if (!_initialized &&
-                GameObject.FindObjectOfType<HSceneProc>() is HSceneProc proc
-                && proc.enabled)
+            if (_active && (!_proc || !_proc.enabled))
+            {
+                // The HProc scene is over, but there may be one more coming.
+                Deactivate();
+            }
+
+            if (!_active &&
+                Manager.Scene.GetRootComponent<HSceneProc>("HProc") is HSceneProc proc &&
+                proc.enabled)
             {
                 _vrMouth = VR.Camera.gameObject.AddComponent<Caress.VRMouth>();
                 AddControllerComponent<Caress.CaressController>();
-                _initialized = true;
+                _proc = proc;
+                _active = true;
             }
         }
+
+        private void Deactivate()
+        {
+            if (_active)
+            {
+                GameObject.Destroy(_vrMouth);
+                DestroyControllerComponent<Caress.CaressController>();
+                _proc = null;
+                _active = false;
+            }
+        }
+
     }
 }

nianqiu-liu/KKS_VR

MainGameVR/Interpreters/HSceneInterpreter.cs
@@ -5,7 +5,8 @@ namespace KKS_VR.Interpreters
 {
     internal class HSceneInterpreter : SceneInterpreter
     {
-        private bool _initialized;
+        private bool _active;
+        private HSceneProc _proc;
         private Caress.VRMouth _vrMouth;
 
         public override void OnStart()
@@ -14,22 +15,36 @@ namespace KKS_VR.Interpreters
 
         public override void OnDisable()
         {
-            if (_initialized)
-            {
-                Object.Destroy(_vrMouth);
-                DestroyControllerComponent<Caress.CaressController>();
-            }
+            Deactivate();
         }
 
         public override void OnUpdate()
         {
-            if (!_initialized &&
-                Object.FindObjectOfType<HSceneProc>() is HSceneProc proc
-                && proc.enabled)
+            if (_active && (!_proc || !_proc.enabled))
+            {
+                // The HProc scene is over, but there may be one more coming.
+                Deactivate();
+            }
+
+            if (!_active &&
+                Manager.Scene.GetRootComponent<HSceneProc>("HProc") is HSceneProc proc &&
+                proc.enabled)
             {
                 _vrMouth = VR.Camera.gameObject.AddComponent<Caress.VRMouth>();
                 AddControllerComponent<Caress.CaressController>();
-                _initialized = true;
+                _proc = proc;
+                _active = true;
+            }
+        }
+
+        private void Deactivate()
+        {
+            if (_active)
+            {
+                Object.Destroy(_vrMouth);
+                DestroyControllerComponent<Caress.CaressController>();
+                _proc = null;
+                _active = false;
             }
         }
     }