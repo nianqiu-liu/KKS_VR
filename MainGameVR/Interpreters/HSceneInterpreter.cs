d4e9761ec6a8ee96cd161ea43d2e70bce198ad31
ovROG <43468716+ovROG@users.noreply.github.com>
2023-11-16 01:17:31 +0300

    [MainGameVR] Use Background Color from H-scene settings (#35)
    Only affects H scenes. Main idea are chroma keying for passthrough. And since game have option for solid bg why not use it.

--- MainGameVR/Interpreters/HSceneInterpreter.cs
+++ MainGameVR/Interpreters/HSceneInterpreter.cs
@@ -9,8 +9,14 @@ namespace KKS_VR.Interpreters
         private HSceneProc _proc;
         private Caress.VRMouth _vrMouth;
 
+        private Color _currentBackgroundColor;
+        private bool _currentShowMap;
+
         public override void OnStart()
         {
+            _currentBackgroundColor = Manager.Config.HData.BackColor;
+            _currentShowMap = Manager.Config.HData.Map;
+            UpdateCameraState();
         }
 
         public override void OnDisable()
@@ -20,6 +26,11 @@ namespace KKS_VR.Interpreters
 
         public override void OnUpdate()
         {
+            if (_currentShowMap != Manager.Config.HData.Map || _currentBackgroundColor != Manager.Config.HData.BackColor)
+            {
+                UpdateCameraState();
+            }
+
             if (_active && (!_proc || !_proc.enabled))
             {
                 // The HProc scene is over, but there may be one more coming.
@@ -41,11 +52,27 @@ namespace KKS_VR.Interpreters
         {
             if (_active)
             {
+                VR.Camera.SteamCam.camera.clearFlags = CameraClearFlags.Skybox;
                 Object.Destroy(_vrMouth);
                 DestroyControllerComponent<Caress.CaressController>();
                 _proc = null;
                 _active = false;
             }
+        }
+
+        private void UpdateCameraState()
+        {
+            if (!Manager.Config.HData.Map)
+            {
+                VR.Camera.SteamCam.camera.backgroundColor = Manager.Config.HData.BackColor;
+                VR.Camera.SteamCam.camera.clearFlags = CameraClearFlags.SolidColor;
+            }
+            else
+            {
+                VR.Camera.SteamCam.camera.clearFlags = CameraClearFlags.Skybox;
+            }
+            _currentBackgroundColor = Manager.Config.HData.BackColor;
+            _currentShowMap = Manager.Config.HData.Map;
         }
     }
 }
